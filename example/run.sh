bd4396acd6d2233c46fbef226ffa22467d9f3967
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2020-01-17 16:17:37 +0100

    Added limb example.

--- example/run.sh
+++ example/run.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 
-# Create observation geomtry...
-../src/nadir nadir.ctl obs.tab
-
 # Create atmospheric data file...
 ../src/climatology nadir.ctl atm.tab
 
+# Create observation geomtry...
+../src/limb nadir.ctl obs_limb.tab
+../src/nadir nadir.ctl obs_nadir.tab
+
 # Call forward model...
-../src/formod nadir.ctl obs.tab atm.tab rad.tab TASK time
+../src/formod nadir.ctl obs_limb.tab atm.tab rad_limb.tab TASK time
+../src/formod nadir.ctl obs_nadir.tab atm.tab rad_nadir.tab TASK time
 
 # Get differences...
 echo -e "\nCheck for differences..."
-diff -sq rad.tab rad.org
-
+diff -sq rad_limb.tab rad_limb.org
+diff -sq rad_nadir.tab rad_nadir.org
