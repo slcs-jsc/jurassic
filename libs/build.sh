8bea013f2fd07fc14b3eb16a02d8e92f9063ae3c
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2024-10-02 21:57:04 +0200

    Update to GSL v2.7.1.

--- libs/build.sh
+++ libs/build.sh
@@ -6,13 +6,13 @@ threads=$(cat /proc/cpuinfo | grep processor | wc -l)
 
 # Prepare directories...
 mkdir -p $target/src $target/bin $target/lib $target/man/man1 \
-    && cp *tar.gz $target/src \
+    && cp *tar.bz2 $target/src \
     && cd $target/src \
-    && for f in $(ls *tar.gz) ; do tar xvzf $f ; done \
+    && for f in $(ls *tar.bz2) ; do tar xvjf $f ; done \
     || exit
 
 # GSL...
-dir=gsl-2.7
+dir=gsl-2.7.1
 cd $target/src/$dir \
     && ./configure --prefix=$target \
     && make -j$threads && make check && make install && make clean \
