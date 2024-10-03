0f4cb02716448a5248d91710b516835ec17e3444
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2024-10-03 14:09:49 +0200

    Update build.sh

--- libs/build.sh
+++ libs/build.sh
@@ -2,18 +2,17 @@
 
 # Setup...
 target=$(rm -rf build && mkdir -p build && cd build && pwd)
-threads=$(cat /proc/cpuinfo | grep processor | wc -l)
 
 # Prepare directories...
 mkdir -p $target/src $target/bin $target/lib $target/man/man1 \
     && cp *tar.bz2 $target/src \
     && cd $target/src \
-    && for f in $(ls *tar.bz2) ; do tar xvjf $f ; done \
+    && for f in *.tar.bz2 ; do tar xvjf $f ; done \
     || exit
 
 # GSL...
 dir=gsl-2.7.1
 cd $target/src/$dir \
     && ./configure --prefix=$target \
-    && make -j$threads && make check && make install && make clean \
+    && make -j && make check && make install && make clean \
     || exit
